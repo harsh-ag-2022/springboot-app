87561f729c4dc4e6e32e084706d3b8674ab4e1a9
harsh-ag-2022 <agrawalharsh.info@gmail.com>
2025-08-29 11:25:44 +0530

    added function

---
+++ java-qualifier/src/main/java/com/bajajfinserv/javaqualifier/dto/SolutionSubmissionRequest.java
@@ -0,0 +1,10 @@
+package com.bajajfinserv.javaqualifier.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SolutionSubmissionRequest {
+    private String finalQuery;
+}
